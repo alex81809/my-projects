a93187279cd7c53289c536c0ddb7ac7c2e243436
Alexandra <asjlobaton@gmail.com>
2025-12-01 14:34:44 +0300

    Create variables.js

---
+++ Web Development/Math/variables.js
@@ -0,0 +1,18 @@
+var x = 10
+var y = 2
+
+// Addition 
+var addition = x + y
+document.getElementById("addition").innerHTML = addition
+
+// Subtraction
+var subtraction = x - y
+document.getElementById("subtraction").innerHTML = subtraction
+
+// Multiplication
+var multiplication = x * y
+document.getElementById("multiplication").innerHTML = multiplication
+
+// Division
+var division = x / y
+document.getElementById("division").innerHTML = division
