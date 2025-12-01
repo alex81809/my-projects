0705fe3f3bb27769ee1504d5d01e988587718e94
Alexandra <asjlobaton@gmail.com>
2025-12-01 12:46:29 +0300

    Create calculator.py

---
+++ Python/Calculator/calculator.py
@@ -0,0 +1,19 @@
+def addition(num1, num2):
+    print("The addition result is: ", num1 + num2)
+
+def subtraction(num1, num2):
+    print("The addition result is: ", num1 - num2)
+
+def multiplication(num1, num2):
+    print("The addition result is: ", num1 * num2)
+
+def division(num1, num2):
+    print("The addition result is: ", num1 / num2)
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+addition(num1, num2)
+subtraction(num1, num2)
+multiplication(num1, num2)
+division(num1, num2)
